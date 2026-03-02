403b920bca2f7aee04bb58058e845d28794a04d7
Ben Malvern <benmalvern@gmail.com>
2026-03-02 15:34:01 -0500

    Started Datapath

---
+++ Phase 1/datapath.v
@@ -0,0 +1,17 @@
+module datapath #(parameter DATA_PATH = 32) ();
+
+	// Control Wires
+    reg [31:0] control;
+    reg [31:0] BusMuxOut;
+
+	
+
+
+
+	register PC, IR;
+	
+		
+	bus BusMux()
+
+	
+	endmodule
